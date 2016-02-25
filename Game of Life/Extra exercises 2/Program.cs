fc60c4a902645e5f7fe460313416fe59aae37a6d
Katrina Rosales <katrinarosales@live.com>
2016-02-25 11:57:18 +0100

    Commitaa

---
+++ Game of Life/Extra exercises 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_exercises_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write first number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Write second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.Write("Write third number: ");
+            int z = int.Parse(Console.ReadLine());
+
+            // villkor
+            // om x större y och x mindre y
+            Console.WriteLine("aaa");
+        }
+
+        bool flag = true;
+        public void CheckCondition(int x, int y, int z, bool flag)
+        {
+
+        }
+    }
+}
