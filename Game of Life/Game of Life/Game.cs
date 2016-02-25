456946ef31c50b9bc8441d4e3a628ce76206818d
Katrina Rosales <katrinarosales@live.com>
2016-02-25 14:15:54 +0100

    Try2

--- Game of Life/Game of Life/Game.cs
+++ Game of Life/Game of Life/Game.cs
@@ -15,9 +15,9 @@ namespace Game_of_Life
         int width;
         int height;
 
-        public Game(int[,] newGrid)
+        public Game(int[,] newGame)
         {
-            generation = (int[,])newGrid.Clone();
+            generation = (int[,])newGame.Clone();
 
             generationCount = 1;
 
@@ -33,6 +33,125 @@ namespace Game_of_Life
             get { return generationCount; }
         }
 
+        private int Neighbours(int x, int y)
+        {
+            int count = 0;
+
+            // Check for x - 1, y - 1
+            // 
+            if (x > 0 && y > 0)
+            {
+                if (generation[y - 1, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x, y - 1
+            if (y > 0)
+            {
+                if (generation[y - 1, x] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y - 1
+            if (x < width - 1 && y > 0)
+            {
+                if (generation[y - 1, x + 1] == 1)
+                    count++;
+            }
+
+            // Check for x - 1, y
+            if (x > 0)
+            {
+                if (generation[y, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y
+            if (x < width - 1)
+            {
+                if (generation[y, x + 1] == 1)
+                    count++;
+            }
+
+            // Check for x - 1, y + 1
+            if (x > 0 && y < height - 1)
+            {
+                if (generation[y + 1, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x, y + 1
+            if (y < height - 1)
+            {
+                if (generation[y + 1, x] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y + 1
+            if (x < width - 1 && y < height - 1)
+            {
+                if (generation[y + 1, x + 1] == 1)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public void WriteNeighbours()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                    Console.Write("{0}", Neighbours(x, y));
+                Console.WriteLine();
+            }
+        }
+
+        public void ProcessGeneration()
+        {
+            int[,] nextGeneration = new int[height, width];
+
+            lastGeneration = (int[,])generation.Clone();
+
+            generationCount++;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (generation[y, x] == 0 && Neighbours(x, y) == 3)
+                        nextGeneration[y, x] = 1;
+                    if (generation[y, x] == 1 &&
+                           (Neighbours(x, y) == 2 || Neighbours(x, y) == 3))
+                        nextGeneration[y, x] = 1;
+                }
+            }
+
+            generation = (int[,])nextGeneration.Clone();
+        }
+
+        public void DrawGeneration()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                    Console.Write("{0}", generation[y, x]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public int AliveCells()
+        {
+            int count = 0;
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    if (generation[y, x] == 1)
+                        count++;
+
+            return count;
+        }
 
     }
 
--- Game of Life/Game of Life/Program.cs
+++ Game of Life/Game of Life/Program.cs
@@ -10,6 +10,53 @@ namespace Game_of_Life
     {
         static void Main(string[] args)
         {
+
+            int[,] grid = new int[,]
+   {
+        { 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,},
+        { 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,},
+        { 0, 1, 1, 0, 1, 1, 1, 0, 0, 0,},
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,},
+        { 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,},
+        { 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,},
+        { 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,},
+        { 1, 1, 0, 0, 1, 0, 0, 0, 0, 0,},
+   };
+
+            Game lifeGrid = new Game(grid);
+
+            Console.WriteLine("Generation 0");
+            lifeGrid.DrawGeneration();
+            Console.WriteLine();
+
+            while (lifeGrid.AliveCells() > 0)
+            {
+                string response;
+
+                Console.WriteLine();
+                Console.WriteLine("Generation {0}", lifeGrid.GenerationCount);
+
+                lifeGrid.ProcessGeneration();
+                lifeGrid.DrawGeneration();
+
+                Console.WriteLine();
+
+                if (lifeGrid.AliveCells() == 0)
+                {
+                    Console.WriteLine("Every one died!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Press <Enter> to contiune or n<Enter> to quit.");
+
+                    response = Console.ReadLine();
+
+                    if (response == "n" || response == "N")
+                        break;
+                }
+            }
         }
     }
 }
+ 
